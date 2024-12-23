53f60e06eb885010eebd57ddbf366a3a9a938060
EdwinRodger <codevaidik@gmail.com>
2024-12-23 22:11:33 +0530

    correctly configured vercel analytics for react

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Analytics } from "@vercel/analytics/react"
 
 import Navbar from './Navbar';
 import Home from './Home';
@@ -15,7 +14,6 @@ const ContactUs = () => <h1 className='text-center mt-5'>Contact Us Page</h1>;
 function App() {
     return (
         <div>
-            <Analytics />
             <Navbar />
             <Router>
                 <Routes>
