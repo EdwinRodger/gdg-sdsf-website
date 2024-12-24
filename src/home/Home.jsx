07831327f9af1d203ddf8aad42e1f23c469c8029
EdwinRodger <codevaidik@gmail.com>
2024-12-24 14:01:08 +0530

    updated paragraph

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -42,7 +42,7 @@ const Home = () => {
                     </div>
                     <div className="col-lg-6">
                         <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">What we have done!</h1>
-                        <p className="lead">Google Developer Student Clubs are university based community groups for students interested in Google developer technologies. Students from all undergraduate or graduate programs with an interest in growing as a developer are welcome. By joining a GDSC, students grow their knowledge in a peer-to-peer learning environment and build solutions for local businesses and their community.</p>
+                        <p className="lead">As Google Developer Groups, we’ve hosted meetups, hands-on workshops, and hackathons to explore technologies like AI, cloud, and mobile development. From networking events to collaborative coding sessions, we’ve connected developers, shared knowledge, and built impactful solutions, fostering innovation and growth within our community.</p>
                     </div>
                 </div>
             </div>
