9f3020ee6d43b7800d8d8866e82eb9c624eb9f54
EdwinRodger <codevaidik@gmail.com>
2024-12-24 11:56:20 +0530

    change class to className

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,40 +3,40 @@ import banner from './assets/banner.png'
 
 const Navbar = () => {
     return (
-        <nav class="navbar navbar-expand-md bg-body-tertiary">
-            <div class="container d-flex justify-content-between">
+        <nav className="navbar navbar-expand-md bg-body-tertiary">
+            <div className="container d-flex justify-content-between">
                 {/* The GDG OC SDSF banner will be displayed on screens larger than 768px */}
-                <div class="d-none d-md-block justify-content-start">
-                    <a class="navbar-brand" href="/">
+                <div className="d-none d-md-block justify-content-start">
+                    <a className="navbar-brand" href="/">
                         <img src={banner} alt="Bootstrap" width="350" />
                     </a>
                 </div>
                 {/* The GDG OC SDSF banner will be displayed on screens smaller than 768px */}
-                <div class="d-block d-md-none">
-                    <a class="navbar-brand" href="/">
+                <div className="d-block d-md-none">
+                    <a className="navbar-brand" href="/">
                         <img src={banner} alt="Bootstrap" width="220" />
                     </a>
                 </div>
                 {/* Toggle button will be displayed on screens smaller than 768px */}
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link" href="/">Home</a>
+                <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">Home</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/events">Events</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/events">Events</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/flashback">Flashback</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/flashback">Flashback</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/team">Team</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/team">Team</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/contact-us">Contact Us</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/contact-us">Contact Us</a>
                         </li>
                     </ul>
                 </div>
