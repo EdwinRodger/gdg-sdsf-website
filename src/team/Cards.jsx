7e8a4eb95bde215aaadecb7b535b17ff59128884
EdwinRodger <codevaidik@gmail.com>
2024-12-25 19:14:00 +0530

    added Cards, reduced redundant code

---
+++ src/team/Cards.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Cards = ({ session, teamData }) => {
+    return (
+        <div>
+            <h1 className='text-center my-5'>{session}</h1>
+            <div className="row row-cols-1 row-cols-md-3 g-4 gap-5 justify-content-center">
+                {teamData.map((member, index) => {
+                    return (
+                        <div className="card" style={{ width: "18rem" }} key={index}>
+                            <img src={member["image"]} className="card-img-top rounded-circle" alt={member["name"]} />
+                            <div className="card-body text-center">
+                                <h5 className="card-title">{member["name"]}</h5>
+                                <p className="card-text">{member["role"]}</p>
+                                <div className="d-flex justify-content-center gap-3">
+                                    {member["github"] === "" ? null : <a href={member["github"]} className="text-body-secondary" target='_blank'><i className="bi bi-github fs-4"></i></a>}
+                                    {member["linkedin"] === "" ? null : <a href={member["linkedin"]} className="text-body-secondary" target='_blank'><i className="bi bi-linkedin fs-4"></i></a>}
+                                    {member["instagram"] === "" ? null : <a href={member["instagram"]} className="text-body-secondary" target='_blank'><i className="bi bi-instagram fs-4"></i></a>}
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default Cards
--- src/team/Team.jsx
+++ src/team/Team.jsx
@@ -1,70 +1,14 @@
 import React from 'react'
+import Cards from './Cards'
 import teamData from './team.json'
 
 const Team = () => {
-    const team24to25 = teamData["Team 2024-25"]
-    const team23to24 = teamData["Team 2023-24"]
-    const team22to23 = teamData["Team 2022-23"]
     return (
         <>
             <div className='container my-5'>
-                <h1 className='text-center my-5'>Team 2024-25</h1>
-                <div className="row row-cols-1 row-cols-md-3 g-4 gap-5 justify-content-center">
-                    {team24to25.map((member, index) => {
-                        return (
-                            <div className="card" style={{ width: "18rem" }} key={index}>
-                                <img src={member["image"]} className="card-img-top rounded-circle" alt={member["name"]} />
-                                <div className="card-body text-center">
-                                    <h5 className="card-title">{member["name"]}</h5>
-                                    <p className="card-text">{member["role"]}</p>
-                                    <div className="d-flex justify-content-center gap-3">
-                                        {member["github"] === "" ? null : <a href={member["github"]} className="text-body-secondary" target='_blank'><i className="bi bi-github fs-4"></i></a>}
-                                        {member["linkedin"] === "" ? null : <a href={member["linkedin"]} className="text-body-secondary" target='_blank'><i className="bi bi-linkedin fs-4"></i></a>}
-                                        {member["instagram"] === "" ? null : <a href={member["instagram"]} className="text-body-secondary" target='_blank'><i className="bi bi-instagram fs-4"></i></a>}
-                                    </div>
-                                </div>
-                            </div>
-                        )
-                    })}
-                </div>
-                <h1 className='text-center my-5'>Team 2023-24</h1>
-                <div className="row row-cols-1 row-cols-md-3 g-4 gap-5 justify-content-center">
-                    {team23to24.map((member, index) => {
-                        return (
-                            <div className="card" style={{ width: "18rem" }} key={index}>
-                                <img src={member["image"]} className="card-img-top rounded-circle" alt={member["name"]} />
-                                <div className="card-body text-center">
-                                    <h5 className="card-title">{member["name"]}</h5>
-                                    <p className="card-text">{member["role"]}</p>
-                                    <div className="d-flex justify-content-center gap-3">
-                                        {member["github"] === "" ? null : <a href={member["github"]} className="text-body-secondary" target='_blank'><i className="bi bi-github fs-4"></i></a>}
-                                        {member["linkedin"] === "" ? null : <a href={member["linkedin"]} className="text-body-secondary" target='_blank'><i className="bi bi-linkedin fs-4"></i></a>}
-                                        {member["instagram"] === "" ? null : <a href={member["instagram"]} className="text-body-secondary" target='_blank'><i className="bi bi-instagram fs-4"></i></a>}
-                                    </div>
-                                </div>
-                            </div>
-                        )
-                    })}
-                </div>
-                <h1 className='text-center my-5'>Team 2022-23</h1>
-                <div className="row row-cols-1 row-cols-md-3 g-4 gap-5 justify-content-center">
-                    {team22to23.map((member, index) => {
-                        return (
-                            <div className="card" style={{ width: "18rem" }} key={index}>
-                                <img src={member["image"]} className="card-img-top rounded-circle" alt={member["name"]} />
-                                <div className="card-body text-center">
-                                    <h5 className="card-title">{member["name"]}</h5>
-                                    <p className="card-text">{member["role"]}</p>
-                                    <div className="d-flex justify-content-center gap-3">
-                                        {member["github"] === "" ? null : <a href={member["github"]} className="text-body-secondary" target='_blank'><i className="bi bi-github fs-4"></i></a>}
-                                        {member["linkedin"] === "" ? null : <a href={member["linkedin"]} className="text-body-secondary" target='_blank'><i className="bi bi-linkedin fs-4"></i></a>}
-                                        {member["instagram"] === "" ? null : <a href={member["instagram"]} className="text-body-secondary" target='_blank'><i className="bi bi-instagram fs-4"></i></a>}
-                                    </div>
-                                </div>
-                            </div>
-                        )
-                    })}
-                </div>
+                <Cards session={"Team 2024-25"} teamData={teamData["Team 2024-25"]} />
+                <Cards session={"Team 2023-24"} teamData={teamData["Team 2023-24"]} />
+                <Cards session={"Team 2022-23"} teamData={teamData["Team 2022-23"]} />
             </div>
         </>
     )
